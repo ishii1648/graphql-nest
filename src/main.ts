51f3582b4f509966c8ddde2944fc79a1b3761be6
Sho Ishii <ishii1492@gmail.com>
2024-02-01 21:19:00 +0900

    change system logger
    Signed-off-by: Sho Ishii <ishii1492@gmail.com>

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConsoleLogger } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    bufferLogs: true,
+  });
+  app.useLogger(new ConsoleLogger());
   await app.listen(3000);
 }
 bootstrap();
